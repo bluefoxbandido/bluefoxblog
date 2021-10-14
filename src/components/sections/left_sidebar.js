967207eed6989db54c6628f6b2e30c9e0ede7853
bluefoxbandido <adamleeparkerjr@outlook.com>
2021-10-13 23:12:26 -0500

    Left Sidebar File Created

---
+++ src/components/sections/left_sidebar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Left_Sidebar extends Component {
+    render() {
+        return (
+            <div>
+                Left Sidebar
+            </div>
+        )
+    }
+}

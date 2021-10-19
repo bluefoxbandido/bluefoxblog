8d2f233703e828e7d14aa4a6eacc9e7794670ff1
bluefoxbandido <adamleeparkerjr@outlook.com>
2021-10-19 17:06:17 -0500

    design changes, added shadow to most elements on home

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -3,21 +3,56 @@ import React from 'react'
 export default function Home() {
     return (
         <div className="home">
-           <div className="home-content">
-           <h5 class="display-1">
-                <span className="blue">{`(`}</span>
-                <span className="red">{`bluefox`}</span>
-                <span className="blue">{`)`}</span>
-                <span className="white">{`=>`}</span>
-                <span className="blue">{'{'}</span>
-                <span className="red">{`dev`}</span>
-                <span className="blue">{`}`}</span>
-            </h5>
-            <h6 class="h6">Creative Writing and Web Development Portfolio</h6>
-            <div className="homeCards">
-                
+            <div className="home-content">
+                <h5 class="display-1">
+                    <span className="blue">{`(`}</span>
+                    <span className="red">{`bluefox`}</span>
+                    <span className="blue">{`)`}</span>
+                    <span className="white">{`=>`}</span>
+                    <span className="blue">{'{'}</span>
+                    <span className="red">{`dev`}</span>
+                    <span className="blue">{`}`}</span>
+                </h5>
+                <h6 class="h6">Creative Writing and Web Development Portfolio</h6>
+                <div className="homeCards">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Brief description of card in paragraph form.</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Brief description of card in paragraph form.</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Brief description of card in paragraph form.</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Brief description of card in paragraph form.</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Brief description of card in paragraph form.</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Brief description of card in paragraph form.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-           </div>
         </div>
     )
 }

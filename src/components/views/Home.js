b70dda436b3fa9d5ef496e7479a19a8a373c7e60
bluefoxbandido <adamleeparkerjr@outlook.com>
2021-10-19 15:41:58 -0500

    worked on content window, still need left sidebar work

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 export default function Home() {
     return (
-        <div>
-            Home
+        <div className="home">
+           <div className="home-content">
+           <h5 class="display-1">
+                <span className="blue">{`(`}</span>
+                <span className="red">{`bluefox`}</span>
+                <span className="blue">{`)`}</span>
+                <span className="white">{`=>`}</span>
+                <span className="blue">{'{'}</span>
+                <span className="red">{`dev`}</span>
+                <span className="blue">{`}`}</span>
+            </h5>
+            <h6 class="h6">Creative Writing and Web Development Portfolio</h6>
+            <div className="homeCards">
+                
+            </div>
+           </div>
         </div>
     )
 }

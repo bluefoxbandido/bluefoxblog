923be3ff0e899b47fee2e002b96ce546544191d8
bluefoxbandido <adamleeparkerjr@outlook.com>
2021-10-21 00:40:28 -0500

    Structured about page. Todo: structure contact and blog page

--- src/components/views/About.js
+++ src/components/views/About.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function About() {
-    return (
-        <div className="about">
-            <h5 class="display-1">
-                <span className="blue">{`(`}</span>
-                <span className="red">{`bluefox`}</span>
-                <span className="blue">{`)`}</span>
-                <span className="white">{`=>`}</span>
-                <span className="blue">{'{'}</span>
-                <span className="red">{`about`}</span>
-                <span className="blue">{`}`}</span>
-            </h5>
-            <h6 class="h6">About the Developer</h6>
-        </div>
-    )
+export default class About extends Component {
+    render() {
+        return (
+            <div className="about">
+                <h5 className="display-1">
+                    <span className="blue">{`(`}</span>
+                    <span className="red">{`bluefox`}</span>
+                    <span className="blue">{`)`}</span>
+                    <span className="white">{`=>`}</span>
+                    <span className="blue">{'{'}</span>
+                    <span className="red">{`about`}</span>
+                    <span className="blue">{`}`}</span>
+                </h5>
+                <h6 className="h6">About the Developer</h6>
+                <div className="bio">
+                    
+                </div>
+            </div>
+        )
+    }
 }
